079d1cac711308e8564fca87a080441ce68562ca
Mohammed Al-Tameemi <altameemi@pm.me>
2025-02-21 23:48:21 -0700

    framework config

--- framework/configuration.nix
+++ framework/configuration.nix
@@ -2,4 +2,5 @@
 { config, pkgs, ... }:
 {
 	system.stateVersion = "24.11";
+  boot.initrd.luks.devices."luks-cf287307-5394-4eb1-8973-4d7067d08036".device = "/dev/disk/by-uuid/cf287307-5394-4eb1-8973-4d7067d08036";
 }
