7b1b1317ca6d869c71989b1dba023106c67adbc6
Mohammed Al-Tameemi <Mohammed Al-Tameemi>
2025-02-21 22:37:57 -0700

    Updated configuration files

---
+++ configuration-nixos.nix
@@ -0,0 +1,9 @@
+{ config, pkgs, ... }:
+{
+	system.stateVersion = "24.11";
+
+	powerManagement.cpuFreqGovernor = "performance";
+	services.udev.extraRules = ''
+		ACTION=="add", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c547", ATTR{power/wakeup}="disabled"
+	'';
+}
