2658bc5b4f1f134cfdd5cc9fa90a8357d01001df
Mohammed Al-Tameemi <altameemi@pm.me>
2025-02-22 00:29:18 -0700

    forced hostname

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
 	system.stateVersion = "24.11";
-
+	networking.hostName = "nixos";
 	powerManagement.cpuFreqGovernor = "performance";
 	services.udev.extraRules = ''
 		ACTION=="add", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c547", ATTR{power/wakeup}="disabled"
